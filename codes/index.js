c9144efc1bb2b45b2aba6024e8055f039747d9fa
WS-SCY <54803931+WS-SCY@users.noreply.github.com>
2021-01-15 01:17:28 +0800

    Add files via upload

---
+++ codes/index.js
@@ -0,0 +1,190 @@
+*{
+	margin: 0px;
+	padding: 0px;
+}
+a:link, a:visited{
+	font-weight: bold;
+	color:#666	;
+	text-align: center;
+	padding: 6px;
+	text-decoration: none;
+	cursor:pointer;
+}
+a:hover, a:active{
+	color:#0080ff;
+	cursor:pointer;
+}
+
+.container{
+	background-color:#f5f6f7; 
+}
+.header{
+	color:#e94e77;
+	width: 100%;
+	height: 60px;  
+	background-color:#a5dff9;  
+	font-weight:bold; 
+} 
+.nav{
+	height:50px; 
+	width:100%;
+	background-color:#a5dff9; 
+	
+}
+.nav_bar{
+	color:#0080ff; 
+	font-weight:bold; 
+}
+.nav_bar a{
+	font-size:40px;
+	
+}
+.content_top{
+	color:#0080ff; 
+	font-weight:bold; 
+	font-size:40px;
+	text-align:center;  
+}
+.content_top p{
+	margin-top:6px;
+}
+.nav_bar img{
+	margin-top:20px;
+	border-radius:30px;
+	display:inline;
+	with:66px;
+	height:66px;
+	float:left;
+	margin-top:0px; 
+}
+.web_name{
+	font-size:40px;
+	float:left;
+}
+.music_control{
+	font-size:30px;
+	float:center;
+}
+.others{ 
+	font-size:30px;
+	float:right;
+	margin-right:40px;
+	
+}
+.guaiqiao_logo img{
+	height: 100%; 
+	display:inline;
+	float:left;
+}
+.content{
+	width:80%;
+	height:750px;
+	background-color:#f5f6f7;
+	float:middle;
+	margin:10px auto;
+}
+.content_top{
+	margin-left:4px;
+	margin-right:4px;
+	width:100%;
+	height:10%;
+	border-top:2px solid #000;
+}
+.content_left{
+	margin-left:3px;
+	width:39%;
+	height:90%;
+	background-color:#FFF;
+	float:left;
+	font-size:36px;
+	text-align:center; 
+	border: 2px solid #000;
+}
+.content_left img{
+	height:350px; 
+}
+.content_left p{
+	
+}
+.content_right{
+	border: 2px solid #000;
+	border-left:0px solid #000;
+	width:60%;
+	height:90%;
+	background-color:#FFF;
+	float:left; 
+	text-align:center;
+	//font-weight:bold;
+}
+.content_right_set{
+	height:60%;
+	width:60%; 
+	margin: 15% auto; 
+	border: 3px;
+	font-size: 66px; 
+	
+}
+
+.content_left_top{
+	width:48%;
+	height:48%;
+	background-color:#0080ff;
+	float:left; 
+	text-align:center;
+	border-radius:40px;
+	margin:1%;
+}
+.content_left_buttom{
+	width:48%;
+	height:48%;
+	background-color:#03a6ff;
+	float:left; 
+	text-align:center;
+	border-radius:40px;
+	margin:1%;
+}
+.content_right_top{
+	width:48%;
+	height:48%;
+	background-color:#1ec0ff;
+	float:left; 
+	text-align:center;
+	border-radius:40px;
+	margin:1%;
+}
+.content_right_buttom{
+	width:48%;
+	height:48%;
+	background-color:#a3daff;
+	float:left; 
+	text-align:center;
+	border-radius:40px;
+	margin:1%;
+}
+.footer{
+	width:auto;
+	height:80px;
+	background-color:#a5dff9;
+	text-align:center;
+	font-size:40px;
+	color:#0080ff;
+	
+}
+.footer a{ 
+	background-color:#a5dff9;
+	text-align:center;
+	font-size:20px;
+	color:#0080ff; 
+}
+#a_left_top{
+	color:#0080ff;
+}
+#a_right_top{
+	color:#03a6ff;
+} 
+#a_left_bottom{
+	color:#1ec0ff;
+} 
+#a_right_bottom{
+	color:#a3daff;
+}
